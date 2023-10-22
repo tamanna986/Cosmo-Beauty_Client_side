bb49224cad211508a4e8a98545a68ca6975c4b05
tamanna986 <tamanna.986jahan@gmail.com>
2023-10-23 00:36:53 +0600

    product deleted oparation added to delete from my cart page

--- src/pages/CartProduct/CartProduct.jsx
+++ src/pages/CartProduct/CartProduct.jsx
@@ -1,19 +1,61 @@
-import {  RiDeleteBin6Line } from "react-icons/ri";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import Swal from 'sweetalert2'
 
-const CartProduct = ({cart}) => {
-    return (
-        <div className="card  bg-base-100 shadow-xl mx-6 md:mx-0">
 
-        <figure><img className="  w-64 h-52"  src={cart.photo} alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title text-rose-900">
-            {cart.name}</h2>
-           <div className="flex gap-10">
-           <p className="bold">Price: {cart.price}</p> <RiDeleteBin6Line></RiDeleteBin6Line>
-           </div>
+const CartProduct = ({ cart }) => {
+  const { _id } = cart;
+
+  const handleDelete = (_id) => {
+    console.log(_id)
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+    
+
+
+
+
+        fetch(`http://localhost:5000/myCart/${_id}`, {
+  method: 'DELETE'
+})
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+  if (data.deletedCount > 0) {
+    Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+  }
+})
+.catch(error => {
+  console.error('Error:', error);
+});
+
+
+
+      }
+    })
+
+
+  }
+  return (
+    <div className="card  bg-base-100 shadow-xl mx-6 md:mx-0">
+      <figure><img className="  w-64 h-52" src={cart.photo} alt="" /></figure>
+      <div className="card-body">
+        <h2 className="card-title text-rose-900">
+          {cart.name}</h2>
+        <div className="flex gap-10">
+          <p className="bold">Price: $ {cart.price}</p> <RiDeleteBin6Line onClick={() => handleDelete(_id)}></RiDeleteBin6Line>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default CartProduct;
--- src/pages/MyCart/MyCart.jsx
+++ src/pages/MyCart/MyCart.jsx
@@ -7,7 +7,7 @@ const MyCart = () => {
     return (
         <div className="mx-auto container my-20 ">
             <h1 className="text-2xl mx-auto text-center text-rose-900 font-bold mb-20">My Cart</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 space-y-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
                      
             {
                 carts.map(cart => 
